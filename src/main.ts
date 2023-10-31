4c10e12df4d6a7e66c52c05977dd5a2180851153
Svetlin Ralchev <iamralch@users.noreply.github.com>
2023-10-31 14:29:05 +0400

    Initial version
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,17 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+async function run() {
+  const token = core.getInput('github-token')
+  const client = github.getOctokit(token)
+  // create a review
+  client.rest.pulls.createReview({
+    owner: github.context.payload.repository!.owner.login,
+    repo: github.context.payload.repository!.name,
+    pull_number: github.context.payload.pull_request!.number,
+    event: 'APPROVE',
+    body: 'LGTM'
+  })
 }
+
+run()
